a75978d1eebba209336aa4d38dd8a6a53cc7b062
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-02-12 15:15:07 -0500

    change autoload

--- conexionBd.js
+++ conexionBd.js
@@ -1,6 +1,6 @@
 import Nedb from "nedb";
 
 
-export const apikeysBd = new  Nedb({filename:"./apikeys.bd",autoload:true});
-export const adminBd = new  Nedb({filename:"./admin.bd",autoload:true});
-export const errorBd = new Nedb({filename:"./error.bd",autoload:true})
+export const apikeysBd = new  Nedb({filename:"./apikeys.bd",});
+export const adminBd = new  Nedb({filename:"./admin.bd"});
+export const errorBd = new Nedb({filename:"./error.bd"})
